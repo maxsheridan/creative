f1d0480c865b392812176864c6f7eae0b05bd736
maxsheridan <max@maxsheridan.com>
2024-12-16 11:36:34 +0200

    Update js, pages

--- js/linkstyling.js
+++
@@ -1,7 +0,0 @@
-var currentPath = window.location.pathname;
-var navLinks = document.querySelectorAll('nav a');
-navLinks.forEach(function(link) {
-  if (link.getAttribute('href') === currentPath) {
-    link.classList.add('active');
-  }
-});
