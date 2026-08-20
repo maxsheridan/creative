d5f389873aa3dfa48e9b33a400129a3ecbbce2f9
maxsheridan <maxsheridan@users.noreply.github.com>
2026-08-20 10:10:39 +0200

    Add utility.css, update build.sh

--- scripts/build-css.sh
+++ scripts/build-css.sh
@@ -4,6 +4,7 @@ set -eu
 
 ROOT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")/.." && pwd)
 CRITICAL_CSS="$ROOT_DIR/assets/css/critical.css"
+UTILITY_CSS="$ROOT_DIR/assets/css/utility.css"
 INDEX_HTML="$ROOT_DIR/index.html"
 
 minify_css() {
@@ -51,8 +52,43 @@ update_critical_css() {
     trap - EXIT INT TERM
 }
 
+update_utility_css() {
+    temporary_file=$(mktemp "${TMPDIR:-/tmp}/creative-utility.XXXXXX")
+    trap 'rm -f "$temporary_file"' EXIT INT TERM
+    minify_css "$UTILITY_CSS" > "$temporary_file"
+    UTILITY_FILE="$temporary_file" ROOT_DIR="$ROOT_DIR" perl -0e '
+        use File::Find;
+
+        open my $utility_fh, "<", $ENV{UTILITY_FILE} or die "$!\n";
+        local $/;
+        my $utility_css = <$utility_fh>;
+        close $utility_fh;
+
+        my $updated = 0;
+        find(sub {
+            return unless -f $_ && /\.html\z/;
+            my $html_file = $File::Find::name;
+            open my $html_fh, "<", $html_file or die "$!\n";
+            my $html = <$html_fh>;
+            close $html_fh;
+
+            my $replaced = $html =~ s{(<style id="utility-css">).*?(</style>)}{$1 . $utility_css . $2}se;
+            if ($replaced) {
+                open my $output_fh, ">", $html_file or die "$!\n";
+                print {$output_fh} $html;
+                close $output_fh;
+                $updated++;
+            }
+        }, $ENV{ROOT_DIR});
+        die "Could not find <style id=\"utility-css\"> in any HTML file\n" unless $updated;
+    '
+    rm -f "$temporary_file"
+    trap - EXIT INT TERM
+}
+
 build() {
     update_critical_css
+    update_utility_css
     write_minified_css "$ROOT_DIR/assets/css/style.css" "$ROOT_DIR/assets/css/style.min.css"
     write_minified_css "$ROOT_DIR/articles/assets/css/style.css" "$ROOT_DIR/articles/assets/css/style.min.css"
     printf '%s\n' "CSS updated."
@@ -64,20 +100,24 @@ file_mtime() {
 
 watch() {
     critical_mtime=$(file_mtime "$CRITICAL_CSS")
+    utility_mtime=$(file_mtime "$UTILITY_CSS")
     root_style_mtime=$(file_mtime "$ROOT_DIR/assets/css/style.css")
     articles_style_mtime=$(file_mtime "$ROOT_DIR/articles/assets/css/style.css")
 
     while :; do
         sleep 1
         new_critical_mtime=$(file_mtime "$CRITICAL_CSS")
+        new_utility_mtime=$(file_mtime "$UTILITY_CSS")
         new_root_style_mtime=$(file_mtime "$ROOT_DIR/assets/css/style.css")
         new_articles_style_mtime=$(file_mtime "$ROOT_DIR/articles/assets/css/style.css")
 
         if [ "$new_critical_mtime" != "$critical_mtime" ] ||
+            [ "$new_utility_mtime" != "$utility_mtime" ] ||
             [ "$new_root_style_mtime" != "$root_style_mtime" ] ||
             [ "$new_articles_style_mtime" != "$articles_style_mtime" ]; then
             build
             critical_mtime=$new_critical_mtime
+            utility_mtime=$new_utility_mtime
             root_style_mtime=$new_root_style_mtime
             articles_style_mtime=$new_articles_style_mtime
         fi
